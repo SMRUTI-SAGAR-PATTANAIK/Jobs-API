5211fd585d79359ca4d55f50d0cc6bed4b61f812
smruti sagar pattanaik <smrutisagarpattanaik1997@gmail.com>
2024-03-09 14:24:57 +0530

    Add swagger

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const express = require('express');
 const app = express();
 
@@ -32,6 +39,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
+
+app.get('/', (req,res) => {
+  res.send('<h1>Jobs API</h1><a href="api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth',authRouter);
